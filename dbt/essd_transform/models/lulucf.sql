b04c1349fa6247b39f3f48b792e389fbbb329b43
MichaelTiemannOSC <mtiemann@os-climate.org>
2022-10-02 16:27:49 +0000

    Put tables into `essd` schema without `essd_` prefix.
    Signed-off-by: MichaelTiemannOSC <mtiemann@os-climate.org>

--- dbt/essd_transform/models/lulucf.sql
+++ dbt/essd_transform/models/lulucf.sql
@@ -1,9 +1,8 @@
 {{ config(materialized='table') }}
-
 with source_data as (
     select year, region_ar6_6, region_ar6_10, blue, blue_units, houghton, houghton_units, oscar, oscar_units, mean, mean_units
     from osc_datacommons_dev.essd.lulucf
 )
-
 select * from source_data
+
 
--- dbt/essd_transform/models/regions.sql
+++ dbt/essd_transform/models/regions.sql
@@ -1,9 +1,8 @@
 {{ config(materialized='table') }}
-
 with source_data as (
     select ISO, name, region_ar6_6, region_ar6_10, region_ar6_22, region_ar6_dev
     from osc_datacommons_dev.essd.regions
 )
-
 select * from source_data
+
 
