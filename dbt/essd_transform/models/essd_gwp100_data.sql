8307ad8a1db7aad8ce7238f2a4a6ccf2e6969cf3
MichaelTiemannOSC <mtiemann@os-climate.org>
2022-10-02 17:43:16 +0000

    Really delete the old `essd_` stuff.  And protect against further zombie models.
    Signed-off-by: MichaelTiemannOSC <mtiemann@os-climate.org>

--- dbt/essd_transform/models/essd_gwp100_data.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-with source_data as (
-    select ISO, country, region_ar6_6, region_ar6_10, region_ar6_22, region_ar6_dev, sector_title, subsector_title, CO2, CO2_units, CH4, CH4_units, N2O, N2O_units, Fgas, Fgas_units, GHG, GHG_units, year
-    from osc_datacommons_dev.mdt_sandbox.essd_gwp100_data
-)
-
-select * from source_data
-
--- dbt/essd_transform/models/essd_lulucf.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-with source_data as (
-    select year, region_ar6_6, region_ar6_10, blue, blue_units, houghton, houghton_units, oscar, oscar_units, mean, mean_units
-    from osc_datacommons_dev.mdt_sandbox.essd_lulucf
-)
-
-select * from source_data
-
