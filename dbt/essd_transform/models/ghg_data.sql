3fe929ca3cc6c5f2a1cd60ef2647d878d501164a
MichaelTiemannOSC <mtiemann@os-climate.org>
2022-10-03 18:30:55 +0000

    Rejigger dbt config info to use views instead of tables.  Other fixes.
    Signed-off-by: MichaelTiemannOSC <mtiemann@os-climate.org>

--- dbt/essd_transform/models/ghg_data.sql
+++ dbt/essd_transform/models/ghg_data.sql
@@ -1,7 +1,7 @@
-{{ config(materialized='table') }}
+{{ config(materialized='view', view_security='invoker') }}
 with source_data as (
     select ISO, country, region_ar6_6, region_ar6_10, region_ar6_22, region_ar6_dev, sector_title, subsector_title, gas, gwp100_ar5, value, value_units, year
-    from osc_datacommons_dev.essd.ghg_data
+    from osc_datacommons_dev.essd.ghg_data_source
 )
 select * from source_data
 
--- dbt/essd_transform/models/sectors.sql
+++ dbt/essd_transform/models/sectors.sql
@@ -1,7 +1,7 @@
-{{ config(materialized='table') }}
+{{ config(materialized='view', view_security='invoker') }}
 with source_data as (
     select EDGAR_code, fossil_bio, sector_title, description, subsector_title, IPCC_2006
-    from osc_datacommons_dev.essd.sectors
+    from osc_datacommons_dev.essd.sectors_source
 )
 select * from source_data
 
